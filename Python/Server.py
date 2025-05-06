096a7611899e95cf83a2b7ad7aeded6bff855c28
Bertram Aakjær <89650492+BertramAakjaer@users.noreply.github.com>
2025-05-06 19:06:13 +0200

    Fixed not running

--- Python/Server.py
+++ Python/Server.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0', 
         port=5000, 
-        debug=True,
+        debug=False,
         extra_files=extra_files,
-        use_reloader=True
+        use_reloader=False
     )
